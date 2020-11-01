326d9fddbbc7e0661bd397777b99a148cd0f8f35
ChoiJuYoung <hajuu96123@naver.com>
2020-11-02 01:37:41 +0900

    WEATHER MODIFY

--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@ from urllib.request import Request
 from urllib import parse
 from bs4 import BeautifulSoup
 
+'''
 def getWeather(where):
     url = "https://search.naver.com/search.naver?query=" + parse.quote(where + "+날씨")
     html = urlopen(url)
@@ -40,4 +41,58 @@ def getWeather(where):
     text1 = (where + "의 기상 정보\n" + "(" + "위치: " + loc + ")" + "\n\n날씨: " + weather  + "음\n" + "최저/최고 기온: " + ltemp + "º / " + htemp + "º\n" + "현재 기온: " + temp + "º\n" + "체감온도: " + ftemp + "º\n" + "습도: " + humid + "%\n" + "풍속: " + wind + "m/s" + "\n오전 강수확률: " + mrain + "%" + "\n오후 강수확률: " + arain + "%" + "\n미세먼지: " + dust + "\n초미세먼지: " + fdust + "\n오존: " + ozone)
     text2 = ("내일(" + t + ") 날씨\n\n" + "오전: " + tmw + ", " + tmt + "º\n" + "강수확률: " + tmr + "%\n" + "오후: " + taw + ", " + tat + "º\n" + "강수확률: " + tar + "%\n\n\n" + "모레(" + dt + ") 날씨\n\n" + "오전: " + datmw + ", " + datmt + "º\n" + "강수확률: " + datmr + "%\n" + "오후: " + dataw + ", " + datat + "º\n" + "강수확률: " + datar + "%")
     
+    return [text1, text2]
+'''
+
+def getWeather(where):
+    url = "https://search.naver.com/search.naver?query=" + parse.quote(where + "+날씨")
+    html = urlopen(url)
+    bsObject = BeautifulSoup(html, "html.parser")
+
+    loc = bsObject.find('div', attrs={'class':'weather_area'}).find('span', attrs={'class':'btn_select'}).text
+    
+    today = bsObject.find('div', attrs={'class': 'today_area'})
+    temp = today.find('span', attrs = {'class': 'todaytemp'}).text + "˚"
+
+    main = today.find('div', attrs={'class' : 'main_info'}).find('ul', attrs={'class': 'info_list'}).find_all('li')
+    weather = main[0].text.split('아')[0]
+    ltemp = main[1].text.split('/')[0].strip()
+    htemp = main[1].text.split('/')[1].split(' ')[0]
+    ftemp = main[1].text.split('체감온도 ')[1]
+
+    sub = today.find('div', attrs={'class': 'sub_info'}).find_all('span', attrs={'class': 'num'})
+    dust = sub[0].text
+    fdust = sub[1].text
+    ozone = sub[2].text
+
+    wind = today.find('div', attrs={'class': 'info_list', 'class': 'wind'}).find('span').text
+    humid = today.find('div', attrs={'class': 'info_list', 'class': 'humidity'}).find('span').text
+
+    mr = bsObject.find_all('span', attrs={'class': 'point_time', 'class': 'morning'})
+    ar = bsObject.find_all('span', attrs={'class': 'point_time', 'class': 'afternoon'})
+    mrain = mr[0].find('span', attrs={'class': 'num'}).text
+    arain = ar[0].find('span', attrs={'class': 'num'}).text
+    tmr = mr[1].find('span', attrs={'class': 'num'}).text
+    tar = ar[1].find('span', attrs={'class': 'num'}).text
+    datmr = mr[2].find('span', attrs={'class': 'num'}).text
+    datar = ar[2].find('span', attrs={'class': 'num'}).text
+
+    tomo = bsObject.find('div', attrs={'class': 'tomorrow_area'}).find_all('div', attrs={'class': 'main_info', 'class':'morning_box'})
+    tmt = tomo[0].find('span', attrs={'class': 'todaytemp'}).text
+    tmw = tomo[0].find('p', attrs={'class': 'cast_txt'}).text
+    tat = tomo[1].find('span', attrs={'class': 'todaytemp'}).text
+    taw = tomo[1].find('p', attrs={'class': 'cast_txt'}).text
+
+    da = bsObject.find('div', attrs={'class': 'day_after'}).find_all('div', attrs={'class': 'main_info', 'class':'morning_box'})
+    datmt = da[0].find('span', attrs={'class': 'todaytemp'}).text
+    datmw = da[0].find('p', attrs={'class': 'cast_txt'}).text
+    datat = da[1].find('span', attrs={'class': 'todaytemp'}).text
+    dataw = da[1].find('p', attrs={'class': 'cast_txt'}).text
+
+    b = bsObject.find_all('li', attrs={'class':'date_info', 'class':'today'})
+    t = b[1].find('span').text.split(" ")[0]
+    dt = b[2].find('span').text.split(" ")[0]
+
+    text1 = (where + "의 기상 정보\n" + "(" + "위치: " + loc + ")" + "\n\n날씨: " + weather  + "음\n" + "최저/최고 기온: " + ltemp + " / " + htemp + "\n" + "현재 기온: " + temp + "\n" + "체감온도: " + ftemp + "\n" + "습도: " + humid + "%\n" + "풍속: " + wind + "m/s" + "\n오전 강수확률: " + mrain + "%" + "\n오후 강수확률: " + arain + "%" + "\n미세먼지: " + dust + "\n초미세먼지: " + fdust + "\n오존: " + ozone)
+    text2 = ("내일(" + t + ") 날씨\n\n" + "오전: " + tmw + ", " + tmt + "º\n" + "강수확률: " + tmr + "%\n" + "오후: " + taw + ", " + tat + "º\n" + "강수확률: " + tar + "%\n\n\n" + "모레(" + dt + ") 날씨\n\n" + "오전: " + datmw + ", " + datmt + "º\n" + "강수확률: " + datmr + "%\n" + "오후: " + dataw + ", " + datat + "º\n" + "강수확률: " + datar + "%")
     return [text1, text2]
