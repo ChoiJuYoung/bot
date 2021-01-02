db9c7e0f95b3a756ad9b4da293ee793dfef31af6
ChoiJuYoung <hajuu96123@naver.com>
2021-01-02 16:55:09 +0900

    s

--- get_mgg.py
+++ get_mgg.py
@@ -2,6 +2,8 @@ from urllib.request import urlopen
 from urllib.request import Request
 from urllib import parse
 from bs4 import BeautifulSoup
+from selenium import webdriver
+
 
 def get_history(name):
     url = "https://maple.gg/u/" + parse.quote(name)
@@ -9,10 +11,21 @@ def get_history(name):
     bsObject = BeautifulSoup(html, "html.parser")
     print(bsObject)
 
+    chk = bsObject.find('button', attrs={'id': 'btn-sync'})
+    if chk.text == '정보갱신':
+        update_history(name)
+
     cody = bsObject.find_all('span', attrs={'class':'character-coord__item-name'})
     for c in cody:
         print(c.text)
 
+def update_history(name):
+    driver = webdriver.Chrome('D:/chromedriver.exe')
+    driver.get('https://maple.gg/u/' + name)
+    btn = driver.find_element_by_id('btn-sync')
+    btn.click()
+
 
 if __name__ == '__main__':
     get_history('FunScream')
+    
